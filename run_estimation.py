40734885ceab86628f829471993e80d96692abb6
Imri Sofer <imrisofer@gmail.com>
2013-02-19 11:23:38 -0500

    CHG: set arguments for run_priors in 'real' mode

--- run_estimation.py
+++ run_estimation.py
@@ -325,6 +325,12 @@ if __name__ == "__main__":
                                               run_type=run_type)
 
         else:
+            if run_priors:
+                include = ['st', 'sv', 'sz', 'z']
+                estimators=['HDDMGamma', 'noninformHDDM', 'ML', 'Quantiles_subj']
+                exp = run_experiments(n_subjs=1, n_trials=[10,20,30,40,50,75,100,150,250], n_params=30, n_datasets=1, equal_seeds=True,
+                                            include=include, view=view, depends_on = {'v':'condition'}, estimators=estimators,
+                                            factor3_vals=[1,2,3], run_type=run_type)
             if run_trials:
                 exp = run_experiments(n_subjs=12, n_trials=[10,20,30,40,50,75,100,150,250], n_params=25, n_datasets=1, equal_seeds=True,
                                             include=include, view=view, depends_on = {'v':'condition'}, estimators=estimators)
